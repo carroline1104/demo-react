66cfc27b9c15d3367d24ffadf17cf048969adaa7
Caroline <>
2025-01-02 18:46:04 +0800

    feat: 调试

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-const jsx = (
+import ReactDOM from 'react-dom';
+const jsxDemo = (
   <div>
-    hello <span>cl-react</span>
+   <span>mo-cl-react</span>
   </div>
 );
+const root = document.querySelector('#root');
+ReactDOM.createRoot(root).render(jsxDemo);
 console.log(React);
-console.log(jsx);
+console.log(jsxDemo);
+console.log(ReactDOM);
 console.log(1234);
